574aa4e765c6169b199787fd237fac21520f6fbc
Pavel <backoffice@rrllc.ru>
2025-04-14 23:04:28 +0400

    add routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,62 @@
-import { ConstructorPage } from '@pages';
-import '../../index.css';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { ConstructorPage, Feed, Login, Register, ForgotPassword, ResetPassword, Profile, ProfileOrders, NotFound404 } from '../../pages';
+import { AppHeader } from '@components';
+import { IngredientDetails, OrderInfo } from '@components';
+import { Modal } from '../modal/modal';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+const App = () => {
+  const navigate = useNavigate();
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+  const handleModalClose = () => {
+    navigate(-1); // Или navigate('/feed') для модалок из ленты заказов
+  };
+
+  return (
+    <BrowserRouter>
+      <div className={styles.app}>
+        <AppHeader />
+        <Routes>
+          <Route path="/" element={<ConstructorPage />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/orders" element={<ProfileOrders />} />
+
+          {/* Модалки */}
+          <Route
+            path="/ingredients/:id"
+            element={
+              <Modal title="Детали ингредиента" onClose={handleModalClose}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path="/feed/:number"
+            element={
+              <Modal title="Детали заказа" onClose={handleModalClose}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path="/profile/orders/:number"
+            element={
+              <Modal title="История заказов" onClose={handleModalClose}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+
+          <Route path="*" element={<NotFound404 />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
