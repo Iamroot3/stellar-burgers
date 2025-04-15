7c34795a1de05d9eb3383d0da93f2e00054c8d0a
Pavel <backoffice@rrllc.ru>
2025-04-16 00:19:50 +0400

    create ingredients, ingredientsSlice.ts and reducer in burger-ingredients

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,61 +1,77 @@
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
-import { ConstructorPage, Feed, Login, Register, ForgotPassword, ResetPassword, Profile, ProfileOrders, NotFound404 } from '../../pages';
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '../../pages';
 import { AppHeader } from '@components';
 import { IngredientDetails, OrderInfo } from '@components';
 import { Modal } from '../modal/modal';
 import styles from './app.module.css';
+import { useEffect } from 'react';
+import { useDispatch } from '../../services/store';
+import { fetchIngredients } from '../../services/slices/ingredientsSlice';
 
 const App = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchIngredients());
+  }, [dispatch]);
 
   const handleModalClose = () => {
     navigate(-1); // Или navigate('/feed') для модалок из ленты заказов
   };
 
   return (
-    <BrowserRouter>
-      <div className={styles.app}>
-        <AppHeader />
-        <Routes>
-          <Route path="/" element={<ConstructorPage />} />
-          <Route path="/feed" element={<Feed />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/profile/orders" element={<ProfileOrders />} />
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/profile/orders' element={<ProfileOrders />} />
 
-          {/* Модалки */}
-          <Route
-            path="/ingredients/:id"
-            element={
-              <Modal title="Детали ингредиента" onClose={handleModalClose}>
-                <IngredientDetails />
-              </Modal>
-            }
-          />
-          <Route
-            path="/feed/:number"
-            element={
-              <Modal title="Детали заказа" onClose={handleModalClose}>
-                <OrderInfo />
-              </Modal>
-            }
-          />
-          <Route
-            path="/profile/orders/:number"
-            element={
-              <Modal title="История заказов" onClose={handleModalClose}>
-                <OrderInfo />
-              </Modal>
-            }
-          />
+        {/* Модалки */}
+        <Route
+          path='/ingredients/:id'
+          element={
+            <Modal title='Детали ингредиента' onClose={handleModalClose}>
+              <IngredientDetails />
+            </Modal>
+          }
+        />
+        <Route
+          path='/feed/:number'
+          element={
+            <Modal title='Детали заказа' onClose={handleModalClose}>
+              <OrderInfo />
+            </Modal>
+          }
+        />
+        <Route
+          path='/profile/orders/:number'
+          element={
+            <Modal title='История заказов' onClose={handleModalClose}>
+              <OrderInfo />
+            </Modal>
+          }
+        />
 
-          <Route path="*" element={<NotFound404 />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+    </div>
   );
 };
 
