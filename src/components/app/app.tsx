8a700a5cf06d8298fc963a7381fbaa33ccfaa815
Pavel <backoffice@rrllc.ru>
2025-04-27 22:54:16 +0400

    fix: delete browser router from app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -11,12 +11,7 @@ import {
 } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useNavigate
-} from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
 
 const App = () => {
