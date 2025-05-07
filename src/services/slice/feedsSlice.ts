930bd5ac3dcd8c056e0e0e1c67bcff80b9494e56
Pavel <backoffice@rrllc.ru>
2025-05-07 21:30:31 +0400

    add feedsSlice

---
+++ src/services/slice/feedsSlice.ts
@@ -0,0 +1,52 @@
+import { getFeedsApi } from '@api';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { TOrder } from '../../utils/types';
+import { RootState } from '../../store';
+
+export type FeedsState = {
+  orders: TOrder[];
+  total: number;
+  totalToday: number;
+  isLoading: boolean;
+  error: string | null;
+};
+
+const initialState: FeedsState = {
+  orders: [],
+  total: 0,
+  totalToday: 0,
+  isLoading: false,
+  error: null
+};
+
+export const fetchFeeds = createAsyncThunk(
+  'feeds/all',
+  getFeedsApi
+);
+
+const feedsSlice = createSlice({
+  name: 'feeds',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchFeeds.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchFeeds.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orders = action.payload.orders;
+        state.total = action.payload.total;
+        state.totalToday = action.payload.totalToday;
+      })
+      .addCase(fetchFeeds.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload as string;
+      });
+  }
+});
+
+export default feedsSlice.reducer;
+export const getFeeds = (state: RootState): IngredientsState =>
+  state.feeds;
