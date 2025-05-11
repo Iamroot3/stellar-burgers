762780c8e885faaa40ef1a8fd0dbb980ad9f06fb
Pavel <backoffice@rrllc.ru>
2025-05-12 00:07:08 +0400

    fix feed.tsx add return and fetchFeeds

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,17 +1,23 @@
 import { Preloader } from '@ui';
 import { FeedUI } from '@ui-pages';
-import { TOrder } from '@utils-types';
-import { FC } from 'react';
-import { getFeeds } from '../../services/slice/feedsSlice';
-import { useSelector } from '../../services/store';
+import { FC, useEffect } from 'react';
+import { useSelector, useDispatch } from '../../services/store';
+import { getFeeds, fetchFeeds } from '../../services/slice/feedsSlice';
 
 export const Feed: FC = () => {
   /** TODO: взять переменную из стора */
-  const { orders } = useSelector(getFeeds);
+  const dispatch = useDispatch();
+  const { orders, total, totalToday, isLoading } = useSelector(getFeeds);
 
-  if (!orders.length) {
+  useEffect(() => {
+    dispatch(fetchFeeds());
+  }, [dispatch]);
+
+  if (isLoading || !orders.length) {
     return <Preloader />;
   }
 
-  <FeedUI orders={orders} handleGetFeeds={() => {}} />;
+  return (
+    <FeedUI orders={orders} handleGetFeeds={() => dispatch(fetchFeeds())} />
+  );
 };
