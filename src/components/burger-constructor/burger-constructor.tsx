38cbca74d2de99219b51b55cc387790561980e02
Pavel <backoffice@rrllc.ru>
2025-05-20 23:11:07 +0400

    fix: add userSlice methods to burger-constructor

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -2,27 +2,49 @@ import { FC, useMemo } from 'react';
 import { TConstructorIngredient } from '@utils-types';
 import { BurgerConstructorUI } from '@ui';
 import { useDispatch, useSelector } from '../../services/store';
+import { useNavigate } from 'react-router-dom';
 import {
   getConstructorBun,
   getConstructorIngredients,
   resetConstructor
 } from '../../services/slice/constructorSlice';
+import {
+  newUserOrder,
+  getLastOrder,
+  getOrderRequestStatus,
+  setLastOrder,
+  getUserAuthStatus
+} from '../../services/slice/userSlice';
 
 export const BurgerConstructor: FC = () => {
   /** TODO: взять переменные constructorItems, orderRequest и orderModalData из стора */
+  const isAuthenticated = useSelector(getUserAuthStatus);
   const constructorIngredients = useSelector(getConstructorIngredients);
   const constructorBun = useSelector(getConstructorBun);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const constructorItems = {
     bun: constructorBun,
     ingredients: constructorIngredients
   };
-  const orderRequest = false;
+  const orderRequest = useSelector(getOrderRequestStatus);
 
-  const orderModalData = null;
+  const orderModalData = useSelector(getLastOrder);
 
   const onOrderClick = () => {
+    if (!isAuthenticated) {
+      return navigate('/login');
+    }
     if (!constructorItems.bun || orderRequest) return;
+    const ingredientsId: string[] = [
+      constructorItems.bun._id,
+      ...constructorItems.ingredients.map(
+        (item: TConstructorIngredient) => item._id
+      )
+    ];
+
+    dispatch(newUserOrder(ingredientsId));
+    dispatch(resetConstructor());
   };
   const closeOrderModal = () => {};
 
